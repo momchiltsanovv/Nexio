01608b289965ceb6e898774fbee8c85878198088
Momchil Tsanov <momchil395@gmail.com>
2025-10-28 18:29:15 +0200

    added NotBlank and refactored

--- src/main/java/com/app/nexio/user/dto/EditUserRequest.java
+++ src/main/java/com/app/nexio/user/dto/EditUserRequest.java
@@ -3,6 +3,8 @@ package com.app.nexio.user.dto;
 import com.app.nexio.user.model.University;
 import com.app.nexio.user.model.User;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.validator.constraints.URL;
@@ -17,25 +19,28 @@ public class EditUserRequest {
 
     private String profilePictureURL;
 
+    @NotBlank
     @Size(min = 2)
-   private String firstName;
+    private String firstName;
 
+    @NotBlank
     @Size(min = 2)
-   private String lastName;
+    private String lastName;
+
+    @Size(max = 255, message = "Major name cant be more than 255 characters")
+    private String major;
+
+    @NotNull
+    private University university;
+
+    @Max(value = 2030, message = "Graduation year must be before 2030 if provided")
+    private Integer graduationYear;
 
     @URL(message = "Instagram URL must be valid if provided")
     private String instagramURL;
 
     @URL(message = "LinkedIn URL must be valid if provided")
     private String linkedinURL;
-
-    private University university;
-
-    @Size(max = 255, message = "Major name cant be more than 255 characters")
-    private String major;
-
-    @Max(value = 2030, message = "Graduation year must be before 2030 if provided")
-    private Integer graduationYear;
 
 
     public static EditUserRequest fromUser(User user) {
